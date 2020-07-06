5d8a804b88417d4302ac87cbaa86030bac07b4bf
Wasiim Hosenbocus <wasiim.hosenbocus@outlook.com>
2020-07-06 13:23:18 +0400

    ***NO_CI***

---
+++ src/services/Product.API/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        //POST - Pass URL and detail
+        // Save into Storage Queue
+        // AZ function download and save image in container, insert record in SQL and update cache
+
+
+        //DEL - Delete Image
+        //Remove from Storage Container and re update cache
+        //check if still in queue and remove
+
+
+        //GET - Get all Details
+        //Get information from Cache
+
+
+    }
+}
