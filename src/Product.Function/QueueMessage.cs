54ba72a54a11e3dbd7a56aabbd372a590405a472
Wasiim Hosenbocus <wasiim.hosenbocus@outlook.com>
2020-07-11 01:08:10 +0400

    added Function App

---
+++ src/Product.Function/QueueMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Function
+{
+    public class QueueMessage
+    {
+        public string URL { get; set; }
+        public string Metadata { get; set; }
+    }
+}
---
+++ src/Product.Function/QueueTrigger.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Product.Function
+{
+    public static class QueueTrigger
+    {
+        [FunctionName("QueueTrigger")]
+        public static void Run([QueueTrigger("product")]string myQueueItem, [Blob("product",System.IO.FileAccess.Write,Connection = "AzureWebJobsStorage")]CloudBlobContainer output, ILogger log)
+        {
+            QueueMessage message = JsonConvert.DeserializeObject<QueueMessage>(myQueueItem);
+
+
+            output.CreateIfNotExistsAsync();
+
+            var blobName = message.URL.Split('/').Last();
+
+            var cloudBlockBlob = output.GetBlockBlobReference(blobName);
+
+
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(message.URL);
+            webRequest.AllowWriteStreamBuffering = true;
+
+            WebResponse webResponse = webRequest.GetResponse();
+
+
+            cloudBlockBlob.UploadFromStream(webResponse.GetResponseStream());
+                
+
+
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
