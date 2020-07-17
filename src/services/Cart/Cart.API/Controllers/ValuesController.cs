d68a38bca9732be40dd54556692269658d33d679
Wasiim Hosenbocus <wasiim.hosenbocus@outlook.com>
2020-07-18 02:01:41 +0400

    returning extra value

--- src/services/Cart/Cart.API/Controllers/ValuesController.cs
+++ src/services/Cart/Cart.API/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Cart.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2","values3" };
         }
 
         // GET api/values/5
